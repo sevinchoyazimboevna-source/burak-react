87041e5a14651f01e156dff5618812d22384a0f0
Angel <sevinchoyazimboevna@gmail.com>
2026-06-26 03:27:50 +0500

    feat: initial setup

---
+++ src/app/App.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+// @ts-ignore: Allow side-effect CSS import without type declarations
+import "../css/app.css";
+import { Box, Button, Container, Stack, Typography } from "@mui/material";
+import { RippleBadge } from "./MaterialTheme/styled";
+
+function App() {
+  return ( 
+    <Container sx={{ backgroundColor: "orange"}}>
+      <Stack direction="column">
+        <Box sx={{my: 4}}>
+          <Typography variant="h4" component={"h4"}>
+            Create React App on TypeScript with REDUX
+          </Typography>
+        </Box>
+        <Box>
+          <RippleBadge badgeContent={4}>
+             <Button variant="contained">Contained</Button>
+          </RippleBadge>
+        </Box>
+      </Stack>
+    </Container>
+  );
+}
+
+export default App;
+
+
+
+
+
