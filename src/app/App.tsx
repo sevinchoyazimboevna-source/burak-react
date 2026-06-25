7739aa0f4372bfa808dc8e3067c06b6607303082
Angel <sevinchoyazimboevna@gmail.com>
2026-06-25 15:17:04 +0900

    feat: MUI customized integration

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,8 +1,25 @@
 import React from "react";
 import "../css/app.css";
+import { Box, Container, Stack, Typography } from "@mui/material";
+import Button from "@mui/material/Button";
+import { RippleBadge } from "./MaterialTheme/styled";
 
 function App() {
-  return <div>HOME</div>;
+  return <Container sx={{ background: "orange" }}>
+    <Stack direction={'column'}>
+      <Box sx={{ my: 4 }}>
+        <Typography variant="h4" component={"h4"}>
+          Initial Setup for SPA
+        </Typography>
+      </Box>
+      <Box>
+        <RippleBadge badgeContent={4}>
+          <Button variant="contained">Contained</Button>
+
+        </RippleBadge>
+      </Box>
+    </Stack>
+  </Container>
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -4,12 +4,19 @@ import { Provider } from "react-redux";
 import { store } from "./app/store";
 import App from "./app/App";
 import reportWebVitals from "./reportWebVitals";
+import CssBaseline from "@mui/material/CssBaseline";
+import { ThemeProvider } from "@mui/material/styles";
+
 import "./css/index.css";
+import theme from "./app/MaterialTheme";
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <App />
+      </ThemeProvider>
     </Provider>
   </React.StrictMode>,
   document.getElementById("root")
