2289beee64db6ce7f8824b1b4583afdb373d3eb9
Angel <sevinchoyazimboevna@gmail.com>
2026-06-27 15:41:00 +0900

    feat: develop header and footer part one

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import "../css/app.css";
 import { Box, Container, Stack, Typography } from "@mui/material";
 import Button from "@mui/material/Button";
 import { RippleBadge } from "./MaterialTheme/styled";
@@ -11,6 +10,9 @@ import { UserPage } from "./screens/userPage";
 import { HomeNavbar } from "./components/headers/HomeNavbar";
 import { OtherNavbar } from "./components/headers/OtherNavbar";
 import { Footer } from "./components/footer";
+import "../css/app.css";
+import '../css/navbar.css'
+import { HelpPage } from "./screens/helpPage";
 
 function App() {
   const location = useLocation();
@@ -29,6 +31,9 @@ function App() {
         </Route>
         <Route path="/member-page">
           <UserPage />
+        </Route>
+        <Route path="/help">
+          <HelpPage />
         </Route>
         <Route path="/">
           <HomePage />
--- src/app/components/headers/HomeNavbar.tsx
+++ src/app/components/headers/HomeNavbar.tsx
@@ -1,5 +1,67 @@
-export function HomeNavbar() { 
-    return (<div>
-        Home Navbar
-    </div>)
+import { Container } from "@mui/material";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import { Stack } from "@mui/system";
+import { NavLink } from "react-router-dom";
+
+export function HomeNavbar() {
+    const authUser = null
+    return (
+        <div className="home-navbar">
+            <Container sx={{ mt: "55px", height: "642px" }}>
+                <Stack direction={"row"} sx={{
+                    minWidth: "700px",
+                    justifyContent: "space-between",
+                    alignItems: "center",
+                }}>
+                    <Box>
+                        <NavLink to="/">
+                            <img src="/icons/burak.svg" style={{ width: "125px", height: "30px" }}></img>
+                        </NavLink>
+                    </Box>
+                    <Stack direction={"row"} sx={{
+                        minWidth: "700px",
+                        justifyContent: "space-between",
+                        alignItems: "center",
+                    }}>
+                        <Box className={"hover-line"}>
+                            <NavLink activeClassName={"underline"} to="/">Home</NavLink>
+                        </Box>
+                        <Box className={"hover-line"}>
+                            <NavLink activeClassName={"underline"} to="/products">Products</NavLink>
+                        </Box>
+                        {authUser ? (
+                            <Box className={"hover-line"}>
+                                <NavLink activeClassName={"underline"} to="/orders">Orders</NavLink>
+                            </Box>
+                        )
+                            : null}
+                        {authUser ? (
+                            <Box className={"hover-line"}>
+                                <NavLink activeClassName={"underline"} to="/member-page">My Page</NavLink>
+                            </Box>
+                        )
+                            : null}
+                        <Box className={"hover-line"}>
+                            <NavLink activeClassName={"underline"} to="/help">Help</NavLink>
+                        </Box>
+
+                        {/* BASKET */}
+
+                        {!authUser ? (
+                            <Box>
+                                <Button variant="contained" style={{ background: "#3776CC", color: "#f8f8ff" }}>
+                                    Login
+                                </Button>
+                            </Box>
+                        ) : (<img />)}
+                    </Stack>
+
+                    <Box>3</Box>
+                </Stack>
+
+                <Stack>DETAIL</Stack>
+            </Container>
+        </div >
+    )
 }
