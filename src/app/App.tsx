fe20c11fa32923752531676c06a2cae5ea6433d8
Angel <sevinchoyazimboevna@gmail.com>
2026-06-27 12:09:53 +0900

    fix: develop screen based components

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,8 +4,10 @@ import { Box, Container, Stack, Typography } from "@mui/material";
 import Button from "@mui/material/Button";
 import { RippleBadge } from "./MaterialTheme/styled";
 import { Switch, Link, Route } from "react-router-dom";
-import { About } from "./screens/About";
-import { Users } from "./screens/User";
+import { HomePage } from "./screens/homePage";
+import { ProductsPage } from "./screens/productsPage";
+import { OrdersPage } from "./screens/ordersPage";
+import { UserPage } from "./screens/userPage";
 
 function App() {
   return (
@@ -13,36 +15,38 @@ function App() {
       <nav>
         <ul>
           <li>
-            <Link to="/">Home</Link>
+            <Link to="/">HomePage</Link>
           </li>
           <li>
-            <Link to="/about">About</Link>
+            <Link to="/products">ProductsPage</Link>
           </li>
           <li>
-            <Link to="/users">Users</Link>
+            <Link to="/orders">OrdersPage</Link>
           </li>
+          <li>
+            <Link to="/member-page">UsersPage</Link>
+          </li>
+
         </ul>
       </nav>
 
       {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
       <Switch>
-        <Route path="/about">
-          <About />
+        <Route path="/products">
+          <ProductsPage />
         </Route>
-        <Route path="/users">
-          <Users />
+        <Route path="/orders">
+          <OrdersPage />
+        </Route>
+        <Route path="/member-page">
+          <UserPage />
         </Route>
         <Route path="/">
-          <Home />
+          <HomePage />
         </Route>
       </Switch>
     </div>)
-}
-
-
-function Home() {
-  return <h2>Home</h2>;
 }
 
 
