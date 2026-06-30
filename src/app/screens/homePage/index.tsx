84e2f13a7d22f9e217c49aa3d6c99a85f91817ba
Angel <sevinchoyazimboevna@gmail.com>
2026-06-30 14:54:11 +0900

    fix: modify statictics sectional component

--- src/app/screens/homePage/index.tsx
+++ src/app/screens/homePage/index.tsx
@@ -4,7 +4,8 @@ import PopularDishes from "./PopularDishes";
 import NewDishes from "./NewDishes";
 import Advertisement from "./Advertisements";
 import ActiveUsers from "./ActiveUsers";
-import Events from "./Events";
+import Events from "./Events"
+import "../../../css/home.css"
 
 export default function HomePage() {
     return <div className={"homepage"} >
