1b4920e3a01e02ff1990c17a03810325ebfec672
Angel <sevinchoyazimboevna@gmail.com>
2026-07-02 01:02:55 +0900

    feat: develop ActiveUsers sectional components

--- src/app/screens/homePage/Advertisements.tsx
+++ src/app/screens/homePage/Advertisements.tsx
@@ -1,9 +1,18 @@
-import { Container } from "@mui/material";
 import React from "react";
+
 export default function Advertisement() {
     return (
-        <div>
-            Advertisement
+        <div className="ads-restaurant-frame">
+            <video
+                className={"ads-video"}
+                autoPlay={true}
+                loop
+                muted
+                playsInline
+                data-video-media=""
+            >
+                <source type="video/mp4" src="video/burak-ads.mp4" />
+            </video>
         </div>
-    )
+    );
 }
