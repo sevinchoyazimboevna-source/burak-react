52214563d80ebb9a95cd7c1283282c9c391f2c9c
Angel <sevinchoyazimboevna@gmail.com>
2026-07-21 02:19:00 +0500

    feat: create config & env variables

---
+++ src/lib/config.ts
@@ -0,0 +1,8 @@
+export const serverApi: string = `${process.env.REACT_APP_API_URL}`;
+
+export const Messages = {
+    error: "Something went wrong!",
+    error2: "Please login first!",
+    error3: "Please fulsill all inputs!",
+    error4: "Only images with jpeg, png, jpg format allowed!",
+};
