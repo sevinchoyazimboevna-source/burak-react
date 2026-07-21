26da71a5c594ea4331db5e042c7743394b4be7c1
Angel <sevinchoyazimboevna@gmail.com>
2026-07-21 05:26:51 +0500

    feat: develop product service business logic and retrieve data from backend

--- src/app/screens/homePage/PopularDishes.tsx
+++ src/app/screens/homePage/PopularDishes.tsx
@@ -9,27 +9,37 @@ import CardOverflow from "@mui/joy/CardOverflow";
 import VisibilityIcon from "@mui/icons-material/Visibility";
 import DescriptionOutlinedIcon from "@mui/icons-material/DescriptionOutlined";
 
-const list = [
-    { productName: "Lavash", imagePath: "/img/lavash.webp" },
-    { productName: "Cutlet", imagePath: "/img/cutlet.webp" },
-    { productName: "Kebab", imagePath: "/img/kebab.webp" },
-    { productName: "Kebab", imagePath: "/img/kebab-fresh.webp" },
-];
+import { useSelector } from 'react-redux'
+import { createSelector } from 'reselect'
+import { retrievePopularDishes } from './selector'
+import { Product } from "../../../lib/types/product";
+import { serverApi } from "../../../lib/config";
+
+/** REDUX SLICE and SELECTOR */
+const popularDishesRetriever = createSelector
+(retrievePopularDishes, (popularDishes) => 
+    ({ popularDishes }));
+
+
+
 
 export default function PopularDishes() {
+    const { popularDishes } = useSelector(popularDishesRetriever);
+
     return (
         <div className="popular-dishes-frame">
             <Container>
                 <Stack className="popular-section">
                     <Box className="category-title">Popular Dishes</Box>
                     <Stack className="cards-frame">
-                        {list.length !== 0 ? (
-                            list.map((ele, index) => {
+                        {popularDishes.length !== 0 ? (
+                            popularDishes.map((ele: Product) => {
+                                const imagePath = `${serverApi}/${ele.productImages[0]}`;
                                 return (
-                                    <CssVarsProvider key={index}>
+                                    <CssVarsProvider key={ele._id}>
                                         <Card className={"card"}>
                                             <CardCover>
-                                                <img src={ele.imagePath} alt="" />
+                                                <img src={imagePath} alt="" />
                                             </CardCover>
                                             <CardCover className={"card-cover"} />
                                             <CardContent sx={{ justifyContent: "flex-end" }}>
@@ -43,13 +53,6 @@ export default function PopularDishes() {
                                                         fontSize="lg"
                                                         textColor="#fff"
                                                         mb={1}
-                                                        sx={{
-                                                            display: "block",
-                                                            overflow: "hidden",
-                                                            textOverflow: "ellipsis",
-                                                            whiteSpace: "nowrap",
-                                                        }}
-                                                        noWrap
                                                     >
                                                         {ele.productName}
                                                     </Typography>
@@ -61,7 +64,7 @@ export default function PopularDishes() {
                                                             display: "flex",
                                                         }}
                                                     >
-                                                        20
+                                                        {ele.productViews}
                                                         <VisibilityIcon
                                                             sx={{ fontSize: 25, marginLeft: "5px" }}
                                                         />
@@ -76,21 +79,14 @@ export default function PopularDishes() {
                                                     px: "var(--Card-padding)",
                                                     borderTop: "1px solid",
                                                     height: "60px",
-                                                    width: "290px",
+                                                    // width: "290px",
                                                 }}
                                             >
                                                 <Typography
                                                     startDecorator={<DescriptionOutlinedIcon />}
                                                     textColor="neutral.300"
-                                                    sx={{
-                                                        display: "block",
-                                                        overflow: "hidden",
-                                                        textOverflow: "ellipsis",
-                                                        whiteSpace: "nowrap",
-                                                    }}
-                                                    noWrap
                                                 >
-                                                    This is delicious meal
+                                                    {ele.productDesc}
                                                 </Typography>
                                             </CardOverflow>
                                         </Card>
--- src/app/screens/homePage/index.tsx
+++ src/app/screens/homePage/index.tsx
@@ -5,35 +5,38 @@ import NewDishes from "./NewDishes";
 import Advertisement from "./Advertisements";
 import ActiveUsers from "./ActiveUsers";
 import Events from "./Events"
-//@ts-ignore
 import "../../../css/home.css"
 
-import { useDispatch, useSelector } from 'react-redux'
+import { useDispatch } from 'react-redux'
 import { Dispatch } from '@reduxjs/toolkit'
-import { createSelector } from 'reselect'
 import { setPopularDishes } from './slice'
-import { retrievePopularDishes } from './selector'
 import { Product } from "../../../lib/types/product";
+import ProductService from "../../services/ProductService";
+import { ProductCollection } from "../../../lib/enums/product.enum";
 
 /** REDUX SLICE and SELECTOR */
 const actionDispatch = (dispatch: Dispatch) => ({
     setPopularDishes: (data: Product[]) => dispatch(setPopularDishes(data)),
-})
-
-const popularDishesRetriever = createSelector(retrievePopularDishes, (popularDishes) => ({ popularDishes }))
+});
 
 export default function HomePage() {
-    const { setPopularDishes } = actionDispatch(useDispatch())
-    const { popularDishes } = useSelector(popularDishesRetriever)
+    const { setPopularDishes } = actionDispatch(useDispatch());
     // Selector: Store => Data
 
 
     useEffect(() => {
         // BACKEND server data request => Data
-
-        // Slice: Data => Store
-        // @ts-ignore
-
+        const product = new ProductService();
+        product.getProducts({
+            page: 1,
+            limit: 4,
+            order: "productViews",
+            productCollection: ProductCollection.DISH,
+        })
+        .then((data) => {
+            setPopularDishes(data);
+        })
+        .catch((err) => console.log(err));
     }, [])
 
 
