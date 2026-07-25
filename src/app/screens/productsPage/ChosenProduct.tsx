f137970d93f82ce152f36d65d439ac55133351c5
Angel <sevinchoyazimboevna@gmail.com>
2026-07-25 17:08:11 +0500

    fix: modify chosenproduct component logic

--- src/app/screens/productsPage/ChosenProduct.tsx
+++ src/app/screens/productsPage/ChosenProduct.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Container, Stack, Box } from "@mui/material";
 import { Swiper, SwiperSlide } from "swiper/react";
 import RemoveRedEyeIcon from "@mui/icons-material/RemoveRedEye";
@@ -7,38 +7,66 @@ import Button from "@mui/material/Button";
 import Rating from "@mui/material/Rating";
 import { useParams } from "react-router-dom"; // @ts-ignore
 import { FreeMode, Navigation, Thumbs } from "swiper";
-//@ts-ignore
 import "swiper/css";
-//@ts-ignore
 import "swiper/css/free-mode";
-//@ts-ignore
 import "swiper/css/navigation";
-//@ts-ignore
 import "swiper/css/thumbs";
 
-import { useDispatch } from 'react-redux'
-import { createSelector, Dispatch } from '@reduxjs/toolkit'
-import { setChosenProduct, setRestaurant } from './slice'
+
+
+import { createSelector, Dispatch } from "@reduxjs/toolkit";
+import { setRestaurant, setChosenProduct, } from "./slice";
 import { Product } from "../../../lib/types/product";
-import { retrievePopularDishes } from "../homePage/selector";
-import { retrieveChosenProduct, retrieveRestaurant} from "./selector";
+import "../../../css/home.css";
+import { retrieveChosenProduct, retrieveRestaurant } from "./selector";
+import ProductService from "../../services/ProductService";
+import MemberService from "../../services/MemberService";
+import { Member } from "../../../lib/types/member";
+import { useDispatch, useSelector } from "react-redux";
+import { useSelect } from "@mui/base";
+import { serverApi } from "../../../lib/config";
 
-/** REDUX SLICE and SELECTOR */
+/** REDUX SLICE & SELECTOR */
 const actionDispatch = (dispatch: Dispatch) => ({
-  setRestaurant: (data: Product[]) => dispatch(setRestaurant(data)),
-    setChosenProduct: (data: Product[]) => dispatch(setChosenProduct(data)),
+  setRestaurant: (data: Member) => dispatch(setRestaurant(data)),
+  setChosenProduct: (data: Product) => dispatch(setChosenProduct(data)),
 });
-
-const chosenProductRetriever = createSelector
-(retrieveChosenProduct, (chosenProduct) => 
-    ({ chosenProduct }));
-
-const restaurantRetriever = createSelector
-(retrieveRestaurant, (restaurant) => 
-    ({ restaurant }));
-
+const chosenProductRetriever = createSelector(
+  retrieveChosenProduct, 
+  (chosenProduct) => ({
+  chosenProduct,
+})
+);
+const restaurantRetriever = createSelector(
+  retrieveRestaurant, 
+  (restaurant) => ({
+  restaurant,
+}));
 
 export default function ChosenProduct() {
+  const {productId} = useParams<{productId: string} > ();
+  const {setRestaurant, setChosenProduct} = actionDispatch(useDispatch());
+  const { chosenProduct } = useSelector(chosenProductRetriever);
+  const { restaurant } = useSelector(restaurantRetriever);
+
+  
+
+  useEffect(() => {
+    const product = new ProductService();
+    product
+    .getProduct(productId)
+    .then(data => setChosenProduct(data))
+    .catch(err => console.log(err));
+
+    const member = new MemberService();
+    member
+    .getRestaurant()
+    .then(data => setRestaurant(data))
+    .catch(err => console.log(err));
+    }, [])
+
+    if(!chosenProduct) return null;
+    
   return (
     <div className={"chosen-product"}>
       <Box className={"title"}>Product Detail</Box>
@@ -51,11 +79,12 @@ export default function ChosenProduct() {
             modules={[FreeMode, Navigation, Thumbs]}
             className="swiper-area"
           >
-            {["/img/cutlet.webp", "/img/kebab-fresh.webp"].map(
+            {chosenProduct?.productImages.map(
               (ele: string, index: number) => {
+                const imagePath = `${serverApi}/${ele}`;
                 return (
                   <SwiperSlide key={index}>
-                    <img className="slider-image" src={ele} />
+                    <img className="slider-image" src={imagePath} />
                   </SwiperSlide>
                 );
               },
@@ -64,23 +93,29 @@ export default function ChosenProduct() {
         </Stack>
         <Stack className={"chosen-product-info"}>
           <Box className={"info-box"}>
-            <strong className={"product-name"}>Kebab</strong>
-            <span className={"resto-name"}>Burak</span>
-            <span className={"resto-name"}>010-2469-4424</span>
+            <strong className={"product-name"}>
+              {chosenProduct?.productName}
+            </strong>
+            <span className={"resto-name"}>{restaurant?.memberNick}</span>
+            <span className={"resto-name"}>{restaurant?.memberPhone}</span>
             <Box className={"rating-box"}>
               <Rating name="half-rating" defaultValue={2.5} precision={0.5} />
               <div className={"evaluation-box"}>
                 <div className={"product-view"}>
                   <RemoveRedEyeIcon sx={{ mr: "10px" }} />
-                  <span>20</span>
+                  <span>{chosenProduct?.productViews}</span>
                 </div>
               </div>
             </Box>
-            <p className={"product-desc"}>No Description</p>
+            <p className={"product-desc"}>
+              {chosenProduct?.productDesc 
+              ? chosenProduct?.productDesc 
+              : "No Description"}
+              </p>
             <Divider height="1" width="100%" bg="#000000" />
             <div className={"product-price"}>
               <span>Price:</span>
-              <span>$12</span>
+              <span>{chosenProduct?.productPrice}</span>
             </div>
             <div className={"button-box"}>
               <Button variant="contained">Add To Basket</Button>
--- src/app/services/MemberService.ts
+++ src/app/services/MemberService.ts
@@ -1,25 +1,42 @@
-import axios from "axios";
 import { serverApi } from "../../lib/config";
-import { Member } from "../../lib/types/member";
+import axios from "axios";
+import { Member } from '../../lib/types/member';
 
 class MemberService {
-  private readonly path: string;
+    private readonly path: string;
 
-  constructor() {
-    this.path = serverApi;
-  }
-
-  public async getTopUsers(): Promise<Member[]> {
-    try {
-      const url = this.path + "/member/top-users";
-      const result = await axios.get(url);
-      console.log("getTopUsers:", result);
-      return result.data;
-    } catch (err) {
-      console.log("Error, getTopUsers:", err);
-      throw err;
+    constructor() {
+        this.path = serverApi;
     }
-  }
+
+    public async getTopUsers(): Promise<[]> {
+        try{
+            const url = this.path + "/member/top-users";
+            const result = await axios.get(url);
+            console.log("getTopUsers", result)
+
+            return result.data;
+        } catch(err) {
+            console.log("Error, getTopUsers:", err)
+            throw err;
+        }
+    }
+
+    public async getRestaurant(): Promise<Member> {
+        try{
+            const url = this.path + "/member/restaurançt";
+            const result = await axios.get(url);
+            console.log("getRestaurant:", result);
+
+            const restauarnt: Member = result.data;
+            return restauarnt;
+        } catch(err) {
+            console.log("Error, getRestaurant:", err)
+            throw err;
+        }
+    }
+
+
 }
 
 export default MemberService;
